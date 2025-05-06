61c6bdc0b40072124131fe5546e6eb8ea877b247
unknown <nikhil1983.be22@chitkara.edu.in>
2025-05-06 16:44:33 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+      <div>
+        <h1>My React App</h1>
+        <h2>Using CI/CD Pipelines</h2>
+        <h3>By Nikhil Verma </h3>
+      </div>
+
+  )
+}
+
+export default App
